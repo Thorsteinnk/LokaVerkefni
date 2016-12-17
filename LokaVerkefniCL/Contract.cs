004057c0ebf144e0c4b5a62f3dbe3751a21066ac
olafurjb.h16 <olafurjb.h16@NTV-1025>
2016-12-17 10:03:03 +0000

    Added contracts to class library

---
+++ LokaVerkefniCL/Contract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokaVerkefniCL
+{
+    class Contract
+    {
+        public int ID { get; set; }
+        public int ApartmentID { get; set; }
+        public Apartment Apartment { get; set; }
+        public int PersonID { get; set; }
+        public Person Person { get; set; }
+        public int Price { get; set; }
+    }
+}
