3ab4f8b64efb7099b7e6afe7940e341b42314372
torsteinnk.h16 <torsteinnk.h16@Nemandi.ntv.is>
2016-12-17 10:03:42 +0000

    Added Persons

---
+++ LokaVerkefniCL/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LokaVerkefniCL
+{
+    class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+    }
+}
